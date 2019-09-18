95650163e28ef3cfda3c5cc55ded5c58cd30c990
Rebecca H <rhernandez@skyslope.com>
2019-09-18 16:41:41 -0700

    api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,38 @@ var path = require("path");
 var fs = require("fs");
 friends = require("./../data/friends.js");
 
-app.get("/api/friends", function (req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
 
-});
+    app.post("/api/friends", function (req, res) {
+        var totDiff;
+        var diffArry = [];
+        var newFriend = req.body;
 
-app.post("/api/friends", function (req, res) {
+        for (var i = 0; i < friends.length; i++) {
+            totDiff = 0;
+            for (var j = 0; j < newFriend.scores.length; j++) {
+                totDiff += Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+            }
+            diffArry.push(totDiff);
+        }
 
-});
+        var match = diffArry.indexOf(Math.min(...diffArry));
+
+        friends.push(newFriend);
+
+        console.log(newFriend);
+
+        fs.readFile(path.join(__dirname, "../data/friends.json"), "utf8", function (err, data) {
+            if (err) throw err;
+            var json = JSON.parse(data);
+            json.push(newFriend);
+            fs.writeFile(path.join(__dirname, "../data/friends.json"), JSON.stringify(json, null, 2), function (err) {
+                if (err) throw err;
+            });
+        });
+        res.json(friends[match]);
+    });
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,11 @@
 var path = require("path");
 
-module.exports = function htmlRoutes(app) {
+module.exports = function (app) {
 
     app.get("/survey", function (req, res) {
-        res.JSON(path.join(__dirname, "/../public/survey.html"));
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
     app.get("/", function (req, res) {
-        res.JSON(path.join(__dirname, "/../public/home.html"));
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 }; 
