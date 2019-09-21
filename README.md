970505ce0b24c90c80e0a39096dd2a8d3b495c98
Rebecca H <rhernandez@skyslope.com>
2019-09-20 19:30:44 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FriendFinder
+
+## This app is not complete. I currently still have to do the following:
--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
